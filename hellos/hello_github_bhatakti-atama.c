3f6e4c3530bf67d5c4152ae5b9eebde0722bbe78
Zeeshan <66550180+ZeeshanK07@users.noreply.github.com>
2022-10-05 23:05:19 +0530

    Added user input
    Added user input to greet the cx according the user also added the greeting line

--- hellos/hello_github_bhatakti-atama.c
+++ hellos/hello_github_bhatakti-atama.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 /* language: c */
 /* Author: bhatakti-atama*/
-  printf("Hello, Github \n");
+  /*Adding user input as a contibution in the code
+  Author: Zeeshan*/
+
+  char username[15];
+  printf("Enter your name:\t");
+  gets(username);
+
+  printf("Hello, %s, thankyou for using this programme\n",username);
   
   return 0;
 }
